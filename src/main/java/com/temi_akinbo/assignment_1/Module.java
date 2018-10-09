776681f1039c7531f8a02f7af96002b5b9a7f223
TemiAkinbo <temi.akinbo01@gmail.com>
2018-10-09 17:23:37 +0100

    First git commit

---
+++ src/main/java/com/temi_akinbo/assignment_1/Module.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.temi_akinbo.assignment_1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Temi
+ */
+class Module {
+    private String moduleName;
+    private String id;
+    private ArrayList<Student> students;
+    private ArrayList<String> courses;
+    
+}
---
+++ src/main/java/com/temi_akinbo/assignment_1/Student.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.temi_akinbo.assignment_1;
+
+import java.util.ArrayList;
+import org.joda.time.DateTime;
+
+/**
+ *
+ * @author Temi
+ */
+
+public class Student {
+    
+    private String name;
+    private int age;    
+    private DateTime dob;
+    private int id;
+    private String username;
+    private ArrayList<String> courses;
+    private ArrayList<Module> regModules;
+    
+    public Student(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public DateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(DateTime dob) {
+        this.dob = dob;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        this.username = this.name + this.age;
+        return username;
+    }
+
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<String> courses) {
+        this.courses = courses;
+    }
+
+    public ArrayList<Module> getRegModules() {
+        return regModules;
+    }
+
+    public void setRegModules(ArrayList<Module> regModules) {
+        this.regModules = regModules;
+    }
+}
